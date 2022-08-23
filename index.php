3c0def64c9b2d8cd7c445455d987ecccffb031ce
Thanuja02 <91487161+Thanuja02@users.noreply.github.com>
2022-08-23 23:00:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,218 @@
-<?php
-$insert = false;
-if(isset($_POST['name'])){
 
-    // Set connection variables
-    $server = "localhost";
-    $username = "madnab";
-    $password = "Thanuja07";
-    $db = "form";
+<?php 
 
-    // Create a database connection
-    $con = new mysqli($server, $username, $password, $db);
+    include("connect.php"); 
 
-    // Check for connection success
-    if ($con->connect_error) {
-        die("Connection failed: " . $con->connect_error);
-    }
+  
 
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+    if (isset($_POST['btn'])) { 
 
-    // database name (form) and then table name (data)
-    $sql = "INSERT INTO data (name, email, message) VALUES ('$name', '$email', '$message');";
+        $date=$_POST['idate']; 
 
-    // Execute the query
-    if($con->query($sql) == true){
-        // Flag for successful insertion
-        $insert = true;
-    }
-    else {
-        echo "Error: " . $sql . "<br>" . $con->error;
-    }
+        $q="select * from grocerytb where Date='$date'"; 
 
-    // Close the database connection
-    $con->close();
-}
-?>
+        $query=mysqli_query($con,$q); 
+
+    }  
+
+    else { 
+
+        $q= "select * from grocerytb"; 
+
+        $query=mysqli_query($con,$q); 
+
+    } 
+?> 
+
+  
+<html> 
+
+  
+<head> 
+
+    <meta http-equiv="Content-Type" 
+
+        content="text/html; charset=UTF-8"> 
+
+  
+
+    <title>View List</title> 
+
+  
+
+    <link rel="stylesheet" href= 
+
+"https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> 
+
+  
+
+    <link rel="stylesheet" 
+
+        href="css/style.css"> 
+</head> 
+
+  
+<body> 
+
+    <div class="container mt-5"> 
+
+          
+
+        <!-- top --> 
+
+        <div class="row"> 
+
+            <div class="col-lg-8"> 
+
+                <h1>View Grocery List</h1> 
+
+                <a href="add.php">Add Item</a> 
+
+            </div> 
+
+            <div class="col-lg-4"> 
+
+                <div class="row"> 
+
+                    <div class="col-lg-8"> 
+
+                          
+
+                        <!-- Date Filtering--> 
+
+                        <form method="post" action=""> 
+
+                            <input type="date" 
+
+                                class="form-control" 
+
+                                name="idate"> 
+
+                          
+
+                            <div class="col-lg-4" 
+
+                                method="post"> 
+
+                                <input type="submit" 
+
+                                class="btn btn-danger float-right" 
+
+                                name="btn" value="filter"> 
+
+                            </div> 
+
+                        </form> 
+
+                    </div> 
+
+                </div> 
+
+            </div> 
+
+        </div> 
+
+  
+
+        <!-- Grocery Cards --> 
+
+        <div class="row mt-4"> 
+
+            <?php 
+
+                while ($qq=mysqli_fetch_array($query))  
+
+                { 
+
+            ?> 
+
+  
+
+            <div class="col-lg-4"> 
+
+                <div class="card"> 
+
+                    <div class="card-body"> 
+
+                        <h5 class="card-title"> 
+
+                            <?php echo $qq['Item_name']; ?> 
+
+                        </h5> 
+
+                        <h6 class="card-subtitle mb-2 text-muted"> 
+
+                            <?php echo 
+
+                            $qq['Item_Quantity']; ?> 
+
+                        </h6> 
+
+                        <?php 
+
+                        if($qq['Item_status'] == 0) { 
+
+                        ?> 
+
+                        <p class="text-info">PENDING</p> 
+
+  
+
+                        <?php 
+
+                        } else if($qq['Item_status'] == 1) { 
+
+                        ?> 
+
+                        <p class="text-success">BOUGHT</p> 
+
+  
+
+                        <?php } else { ?> 
+
+                        <p class="text-danger">NOT AVAILABLE</p> 
+
+  
+
+                        <?php } ?> 
+
+                        <a href= 
+
+                        "delete.php?id=<?php echo $qq['Id']; ?>" 
+
+                            class="card-link"> 
+
+                            Delete
+
+                        </a> 
+
+                        <a href= 
+
+                        "update.php?id=<?php echo $qq['Id']; ?>" 
+
+                            class="card-link"> 
+
+                            Update 
+
+                        </a> 
+
+                    </div> 
+
+                </div><br> 
+
+            </div> 
+
+            <?php 
+
+            } 
+
+            ?> 
+
+        </div> 
+
+    </div> 
+</body> 
+
+  
+</html>
+
